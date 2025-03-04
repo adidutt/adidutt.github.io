c0c9a4966bd6bf5596c089730541ec131197b3c8
adidutt <53310020+adidutt@users.noreply.github.com>
2025-03-03 23:36:59 -0500

    Fixed the dark theme bug

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"], // Adjust this to match your file structure
+    darkMode: "class", // Ensure dark mode uses the 'dark' class
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
